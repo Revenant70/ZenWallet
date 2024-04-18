3f895488b0be8aac7b603932c29a861a3ef7ff3b
Revenant70 <jackmcg777@protonmail.com>
2024-04-18 01:01:14 -0400

    Feat: home-page header format

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
 
   return (
-    <>
-      Hello world
-    </>
+    <div className="bg-[#181a1f] h-screen w-screen  text-[#ffffff]">
+      <Header />
+      <Home />
+    </div>
   )
 }
 
