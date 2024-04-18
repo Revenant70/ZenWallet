ce0a46724d919610bd4b9f000d3b9bb691da31a4
Revenant70 <jackmcg777@protonmail.com>
2024-04-18 13:05:06 -0400

    Reset

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,10 @@
-import Header from "./components/Header";
-import Home from "./components/Home";
+
 
 function App() {
 
   return (
-    <div className="bg-[#181a1f] h-screen w-screen  text-[#ffffff]">
-      <Header />
-      <Home />
-    </div>
+    <>
+    </>
   )
 }
 
